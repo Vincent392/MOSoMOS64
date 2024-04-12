bdc698a96c1e82be7b0739d66c25401d252f0c8b
Vincent392 <90470156+Vincent392@users.noreply.github.com>
2024-04-12 16:26:53 +0000

    Update setup-arm.sh

--- setup-arm.sh
+++ setup-arm.sh
@@ -3,3 +3,5 @@ echo I AM *NOT* LIABLE IF YOU BREAK YOUR SYSTEM RUNNING THIS.
 Read junk
 echo install Rosetta
 softwareupdate --install-rosetta
+echo re-enable 32 bit execution from Darwin
+sudo nvram boot-args=”no32exec=0″
