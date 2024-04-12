58179e393991027d33a9da13fdfded167832d154
Vincent392 <90470156+Vincent392@users.noreply.github.com>
2024-04-12 16:22:56 +0000

    Update

---
+++ setup.sh
@@ -0,0 +1,5 @@
+echo PLEASE, MAKE A BACKUP BEFORE RUNNING.
+echo I AM *NOT* LIABLE IF YOU BREAK YOUR SYSTEM RUNNING THIS.
+Read junk
+echo re-enable 32 bit execution from Darwin
+sudo nvram boot-args=”no32exec=0″
